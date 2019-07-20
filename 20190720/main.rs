056ac8f744cdb1ae52083350bc1dac319c862802
mHALr <ryo97321@gmail.com>
2019-07-20 11:30:49 +0900

    while

--- 20190720/main.rs
+++ 20190720/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-    loop {
-        println!("Loop forever!");
+    let mut x = 5;
+    let mut done = false;
+
+    while !done {
+        x += x - 3;
+
+        println!("{}", x);
+
+        if x % 5 == 0 {
+            done = true;
+        }
     }
 }
