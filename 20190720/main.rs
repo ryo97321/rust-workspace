975c367f333dad698a9a687fa49375f83aa96219
mHALr <ryo97321@gmail.com>
2019-07-20 11:35:56 +0900

    loop-rabel

--- 20190720/main.rs
+++ 20190720/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    for (i, j) in (5..10).enumerate() {
-        println!("i = {} and j = {}", i, j);
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 { continue 'outer; }
+            if y % 2 == 0 { continue 'inner; }
+            println!("x: {}, y: {}", x, y);
+        }
     }
 }
