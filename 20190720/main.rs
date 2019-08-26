fadda0f17636ea488690c4febf14680f5e3c1002
mHALr <ryo97321@gmail.com>
2019-08-26 10:15:16 +0900

    changed

--- 20190720/main.rs
+++ 20190720/main.rs
@@ -1,14 +1,5 @@
-struct Foo<'a> {
-    x: &'a i32,
-}
-
-impl<'a> Foo<'a> {
-    fn x(&self) -> &'a i32 { self.x }
-}
-
 fn main() {
-    let y = &5;
-    let f = Foo { x: y };
-
-    println!("x is {}", f.x());
+    let mut x = 5;
+    x = 6;
+    println!("x is {}", x);
 }
