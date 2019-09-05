bce9bc4baa12fb264136050390e656226056a401
m0ntBL4Nc <ryo97321@gmail.com>
2019-09-05 18:10:53 +0900

    add project files

---
+++ 20190905/loop_test/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 { continue 'outer; }
+            if y % 2 == 0 { continue 'inner; }
+            println!("x: {}!, y: {}!", x, y);
+        }
+    }
+}
