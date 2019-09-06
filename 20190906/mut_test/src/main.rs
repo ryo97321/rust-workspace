c2696bdc983829c79f49b7b8dd4db7e410c38646
m0ntBL4Nc <ryo97321@gmail.com>
2019-09-06 17:03:32 +0900

    add mut_test

---
+++ 20190906/mut_test/src/main.rs
@@ -0,0 +1,14 @@
+use std::cell::Cell;
+
+struct Point {
+    x: i32,
+    y: Cell<i32>,
+}
+
+fn main() {
+    let point = Point { x: 5, y: Cell::new(6) };
+
+    point.y.set(7);
+
+    println!("y: {:?}", point.y);
+}
