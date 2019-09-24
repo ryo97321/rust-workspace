82be606d4101a0ab5d8c125582ad5b44e30e966a
mHALr <ryo97321@gmail.com>
2019-09-24 10:32:47 +0900

    today first commit

---
+++ 20190924/rectangles/src/main.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
