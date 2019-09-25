ec6fc82f6d533f0979f133604dcfeb17289c43d1
mHALr <ryo97321@gmail.com>
2019-09-25 20:46:09 +0900

    add no48

---
+++ yukicoder/level1/no48.rs
@@ -0,0 +1,130 @@
+#[derive(Debug)]
+struct Robot {
+    x: i32,
+    y: i32,
+    direction: String,
+}
+
+impl Robot {
+    fn new() -> Robot {
+        Robot {
+            x: 0,
+            y: 0,
+            direction: String::from("N"),
+        }
+    }
+
+    fn move_x(&mut self, dx: i32) {
+        self.x += dx;
+    }
+
+    fn move_y(&mut self, dy: i32) {
+        self.y += dy;
+    }
+
+    fn rotate_90_degree(&mut self) {
+        let now = &self.direction;
+        if *now == String::from("N") {
+            self.direction = String::from("E");
+        } else if *now == String::from("E") {
+            self.direction = String::from("S");
+        } else if *now == String::from("S") {
+            self.direction = String::from("W");
+        } else if *now == String::from("W") {
+            self.direction = String::from("N")
+        }
+    }
+
+    fn rotate_minus_90_degree(&mut self) {
+        let now = &self.direction;
+        if *now == String::from("N") {
+            self.direction = String::from("W");
+        } else if *now == String::from("W") {
+            self.direction = String::from("S");
+        } else if *now == String::from("S") {
+            self.direction = String::from("E");
+        } else if *now == String::from("E") {
+            self.direction = String::from("N")
+        }
+    }
+}
+
+fn getline() -> String {
+    let mut __ret = String::new();
+    std::io::stdin().read_line(&mut __ret).ok();
+    return __ret;
+}
+
+fn main() {
+    let x_goal: i32 = getline().trim().parse().unwrap();
+    let y_goal: i32 = getline().trim().parse().unwrap();
+    let robot_can_move_distance: i32 = getline().trim().parse().unwrap();
+
+    let mut remain_x: i32 = x_goal.abs();
+    let mut remain_y: i32 = y_goal.abs();
+
+    let mut order_count: i32 = 0;
+    let mut robot = Robot::new();
+
+    // 1. 最初の方向を決める
+    if y_goal < 0 {
+        if x_goal > 0 {
+            robot.rotate_90_degree();
+        } else if x_goal < 0 {
+            robot.rotate_minus_90_degree();
+        }
+        order_count += 1;
+    }
+
+    // 2. 進行する
+    if robot.direction == String::from("N") {
+        loop {
+            if remain_y - robot_can_move_distance <= 0 {
+                robot.move_y(remain_y);
+            } else {
+                robot.move_y(robot_can_move_distance);
+            }
+            remain_y -= robot_can_move_distance;
+            order_count += 1;
+
+            if remain_y <= 0 {
+                break;
+            }
+        }
+    } else if robot.direction == String::from("E") {
+        loop {
+            if remain_x - robot_can_move_distance <= 0 {
+                robot.move_x(remain_x);
+            } else {
+                robot.move_x(robot_can_move_distance);
+            }
+            remain_x -= robot_can_move_distance;
+            order_count += 1;
+
+            if remain_x <= 0 {
+                break;
+            }
+        }
+    } else if robot.direction == String::from("W") {
+        loop {
+            if remain_x - robot_can_move_distance <= 0 {
+                robot.move_x(remain_x * -1);
+            } else {
+                robot.move_x(robot_can_move_distance * -1);
+            }
+            remain_x -= robot_can_move_distance;
+            order_count += 1;
+
+            if remain_x <= 0 {
+                break;
+            }
+        }
+    }
+
+    // 3. 回転する
+
+    // 4. 残りの分進行する
+
+    println!("{:?}", robot);
+    println!("order_count : {}", order_count);
+}
