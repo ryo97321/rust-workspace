7b9a665a1d7d7f724b64d8f47c2816ebfb1d79f8
mHALr <ryo97321@gmail.com>
2019-09-29 08:50:08 +0900

    finished no56

--- yukicoder/level1/no56.rs
+++ yukicoder/level1/no56.rs
@@ -1,11 +1,16 @@
 fn getline() -> String {
-    let mut __ret == String::new();
+    let mut __ret = String::new();
     std::io::stdin().read_line(&mut __ret).ok();
     return __ret;
 }
 
 fn main() {
     let line = getline();
-    let params: Vec<_> = line.trim().split(' ').correct();
+    let params: Vec<_> = line.trim().split(' ').collect();
     let d: f64 = params[0].parse().unwrap();
+    let p: f64 = params[1].parse().unwrap();
+
+    let result = ((d * (p / 100.0)) + d) as i32;
+
+    println!("{}", result);
 }
