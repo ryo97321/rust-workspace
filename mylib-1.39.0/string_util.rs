bdd6639bd33a46a24819ee0a1c7b67d671536316
mHALr <ryo97321@gmail.com>
2019-10-14 14:59:52 +0900

    rename mylib-1.39.0/ -> mylib_1.39.0/

--- mylib-1.39.0/string_util.rs
+++
@@ -1,9 +0,0 @@
-#![crate_type = "lib"]
-#![crate_name = "string_util"]
-
-// 1行取得する
-pub fn getline() -> String {
-    let mut __ret = String::new();
-    std::io::stdin().read_line(&mut __ret).ok();
-    return __ret;
-}
