bf3887d74c1f54302c53cb53f84708d648fb3096
m0ntbL4Nc <ryo97321@gmail.com>
2019-10-18 14:04:03 +0900

    add insertation sort program

--- sort_practice/insertation_sort/src/main.rs
+++ sort_practice/insertation_sort/src/main.rs
@@ -1,3 +1,32 @@
+fn insertation_sort(v: &mut Vec<i32>) {
+    for i in 1..v.len() {
+        let mut j = i;
+
+        loop {
+            if j <= 0 {
+                break;
+            }
+
+            if v[j-1] > v[j] {
+                let temp = v[j-1];
+                v[j-1] = v[j];
+                v[j] = temp;
+            }
+
+            j -= 1;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut v: Vec<i32> = Vec::new();
+    v.push(3);
+    v.push(1);
+    v.push(5);
+    v.push(2);
+    v.push(99);
+    println!("{:?}", v);
+
+    insertation_sort(&mut v);
+    println!("{:?}", v);
 }
