f371bbbd87a737cde72cdc5180a7f70b7b679cb1
mHALr <ryo97321@gmail.com>
2019-10-22 10:56:45 +0900

    add no279 (finished)

--- yukicoder/level1/no279.rs
+++ yukicoder/level1/no279.rs
@@ -8,48 +8,36 @@ fn main() {
     let line = getline();
     let s = line.trim();
 
-    let mut char_use_table: Vec<bool> = Vec::new();     // 使用済->true 未使用->false
-    for _ in 0..s.len() {
-        char_use_table.push(false);
-    }
-
-    let mut tree_chars: Vec<char> = Vec::new();
-    tree_chars.push('t');
-    tree_chars.push('r');
-    tree_chars.push('e');
-    tree_chars.push('e');
-    let mut tree_chars_index: usize = 0;
-
-    let mut valid_chars: Vec<char> = Vec::new();
-    for c in s.chars().into_iter() {
-        if c == 't' || c == 'r' || c == 'e' {
-            valid_chars.push(c);
-        }
-    }
-
     let mut tree_count = 0;     // 何個treeができたか
-    loop {
-        let mut can_make_tree = false;
 
-        for _ in 0..4 {
-            let mut s_index = 0;
-            for c in s.chars().into_iter() {
-                if char_use_table[s_index] == false && c == tree_chars[tree_chars_index] {
-                    tree_chars_index += 1;
-                    if tree_chars_index >= tree_chars.len() {
-                        can_make_tree = true;
-                        tree_chars_index = 0;
-                    }
-                    char_use_table[s_index] = true;
-                    break;
-                }
-                s_index += 1;
-            }
+    let mut t_vector: Vec<char> = Vec::new();
+    let mut r_vector: Vec<char> = Vec::new();
+    let mut e_vector: Vec<char> = Vec::new();
+
+    for c in s.chars().into_iter() {
+        if c == 't' {
+            t_vector.push(c);
+        } else if c == 'r' {
+            r_vector.push(c);
+        } else if c == 'e' {
+            e_vector.push(c);
+        }
+    }
+
+    let mut t_vector_index: usize = 0;
+    let mut r_vector_index: usize = 0;
+    let mut e_vector_index: usize = 0;
+
+    loop {
+        if t_vector_index < t_vector.len() && r_vector_index < r_vector.len() && e_vector_index+1 < e_vector.len() {
+            tree_count += 1;
         }
 
-        if can_make_tree == true {
-            tree_count += 1;
-        } else {
+        t_vector_index += 1;
+        r_vector_index += 1;
+        e_vector_index += 2;
+
+        if t_vector_index >= t_vector.len() || r_vector_index >= r_vector.len() || e_vector_index >= e_vector.len() {
             break;
         }
     }
