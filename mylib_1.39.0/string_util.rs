1ed66bc98f82e93871d43abd3ddc2cbdc645fa48
mHALr <ryo97321@gmail.com>
2019-11-01 13:51:07 +0900

    add reverse_str function

--- mylib_1.39.0/string_util.rs
+++ mylib_1.39.0/string_util.rs
@@ -7,3 +7,19 @@ pub fn getline() -> String {
     std::io::stdin().read_line(&mut __ret).ok();
     return __ret;
 }
+
+// Stringを逆にして返す（ex. test -> tset）
+pub fn reverse_str(input: &String) -> String {
+    let mut reversed = String::new();
+    let mut chars: Vec<char> = Vec::new();
+
+    for c in input.chars().into_iter() {
+        chars.push(c);
+    }
+
+    for i in (0..chars.len()).rev() {
+        reversed += &chars[i].to_string();
+    }
+
+    return reversed;
+}
