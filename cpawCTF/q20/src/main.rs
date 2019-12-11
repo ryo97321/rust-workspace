11e6465b118b6996d91f1695533b273cc71c5cf2
mHALr <ryo97321@gmail.com>
2019-12-11 21:59:31 +0900

    finished and add comment

--- cpawCTF/q20/src/main.rs
+++ cpawCTF/q20/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    // 暗号化されたフラグ
+    let e_flag = vec!["r", "u", "o", "Y", "c", "e", "d", "_", "e", "h", "p", "i", "g", "n", "i", "r", "i", "k", "s", "_", "i", "_", "l", "l", "r", "g", "_", "s", "t", "a", "e"];
+
+    for key in 1..e_flag.len() {
+        let mut i: usize = 0;
+        let mut flag = String::new();
+
+        loop {
+            for j in (i..(i+key)).rev() {
+                if j >= e_flag.len() {
+                    break;
+                } else {
+                    flag += &e_flag[j];
+                }
+            }
+            i += key;
+
+            // 残りを表示してbreak
+            if i > e_flag.len() {
+                i -= key;
+                for j in (i..e_flag.len()).rev() {
+                    flag += &e_flag[j];
+                }
+                break;
+            }
+        }
+
+        // 結果を表示
+        println!("{}:{}", key, flag);
+    }
 }
