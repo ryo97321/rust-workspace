e383270879e29e64b2b8fcb2386e550e0cd22f8c
mHALr <ryo97321@gmail.com>
2019-12-14 08:30:40 +0900

    add calc sample

--- cpawCTF/q29/bigint_test/src/main.rs
+++ cpawCTF/q29/bigint_test/src/main.rs
@@ -1,25 +1,36 @@
-extern crate num_bigint;
-extern crate num_traits;
-
-use num_bigint::BigUint;
-use num_traits::{Zero, One};
-
-// Calculate large fibonacci numbers.
-// fn fib(n: usize) -> BigUint {
-//     let mut f0: BigUint = Zero::zero();
-//     let mut f1: BigUint = One::one();
-//     for _ in 0..n {
-//         let f2 = f0 + &f1;
-//         // This is a low cost way of swapping f0 with f1 and f1 with f2.
-//         f0 = replace(&mut f1, f2);
-//     }
-//     f0
-// }
+use num_bigint::BigInt;
 
 fn main() {
-    // This is a very large number.
-    // println!("fib(1000) = {}", fib(1000));
+    let s = "10000000000000000";
+    let s2 = "9999999999999998";
 
-    let mut number: BigUint = Zero::zero();
-    number += 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+    let n: BigInt = s.parse().unwrap();
+    let n2: BigInt = s2.parse().unwrap();
+    let mut n3: BigInt;
+    n3 = n2.clone();
+
+    println!("n = {}", n);
+    println!("n2 = {}", n2);
+    println!("n3 = {}", n3);
+
+    // Copyトレイトが実装されていないため, &nのように参照を使用
+    let add_result = &n + &n2;
+    let diff_result = &n - &n2;
+    let mul_result = &n * &n2;
+    let div_result = &n / &n2;
+    let remain_result = &n % &n2;
+
+    if n > n2 {
+        println!("n > n2");
+    } else if n < n2 {
+        println!("n < n2");
+    } else if n == n2 {
+        println!("n == n2");
+    }
+    
+    println!("{} + {} = {}", n, n2, add_result);
+    println!("{} - {} = {}", n, n2, diff_result);
+    println!("{} * {} = {}", n, n2, mul_result);
+    println!("{} / {} = {}", n, n2, div_result);
+    println!("{} % {} = {}", n, n2, remain_result);
 }
