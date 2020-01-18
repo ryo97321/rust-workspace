eb4ce613cc590f33027dfddfdfec4af2bb27743f
mHALr <ryo97321@gmail.com>
2020-01-18 22:08:39 +0900

    change code

--- 20200118/sample/src/main.rs
+++ 20200118/sample/src/main.rs
@@ -1,15 +1,29 @@
-enum TYPE {
-    TYPE_A(i32, u32, usize),
-    TYPE_B{a: char, b: i32},
-    TYPE_C,
+struct A;
+struct B;
+
+trait C {
+    type TMP;
+    fn Add(self: Self, x: Self::TMP, y: Self::TMP) -> Self::TMP;
+}
+
+impl C for A {
+    type TMP = i32;
+    fn Add(self: A, x: i32, y: i32) -> i32 {
+        x + y
+    }
+}
+
+impl C for B {
+    type TMP = String;
+    fn Add(self: B, x: String, y: String) -> String {
+        x + &y
+    }
 }
 
 fn main() {
-    let t: TYPE = TYPE::TYPE_B{ a:'b', b:10 };
+    let a: A = A{};
+    let b: B = B{};
 
-    match t {
-        TYPE::TYPE_A(a, b, c) => { println!("{}, {}, {}", a, b, c); },
-        TYPE::TYPE_B{a, b} => { println!("{}, {}", a, b); },
-        TYPE::TYPE_C => { println!("TYPE_C"); },
-    };
+    println!("add = {}", a.Add(10, 15));
+    println!("add = {}", b.Add("hello ".to_string(), "world".to_string()));
 }
