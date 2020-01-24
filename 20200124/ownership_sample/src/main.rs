e097f6a3d247f31de723866794761eae49aac80b
mHALr <ryo97321@gmail.com>
2020-01-24 13:16:29 +0900

    add 20200124/

---
+++ 20200124/ownership_sample/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = true;
+
+    let _y = change_truth(a);
+
+    println!("{}", a);
+}
+
+fn change_truth(x: bool) -> bool {
+    !x
+}
---
+++ 20200124/reference_sample/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let y: &i32;
+    let x = 5;
+    y = &x;
+
+    println!("{}", y);
+}
