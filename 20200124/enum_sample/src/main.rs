7092fdb48064add03ad1d4b80d8311f21de26016
mHALr <ryo97321@gmail.com>
2020-01-24 13:36:06 +0900

    add enum_sample/

---
+++ 20200124/enum_sample/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    enum Message {
+        Quit,
+        ChangeColor(i32, i32, i32),
+        Move{ x: i32, y: i32 },
+        Write(String),
+    }
+
+    let x: Message = Message::Move { x: 3, y: 4 };
+
+    enum BoardGameTurn {
+        Move { squares: i32 },
+        Pass,
+    }
+
+    let y: BoardGameTurn = BoardGameTurn::Move { squares: 1 };
+}
